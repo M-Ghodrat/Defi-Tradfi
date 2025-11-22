642b3b5cf142204b432ca56d9e481f05be70e8d9
M-Ghodrat <79672718+M-Ghodrat@users.noreply.github.com>
2025-11-22 14:35:23 -0800

    Update data_loader.py

--- logic/data_loader.py
+++ logic/data_loader.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def load_csv(file):
-    return pd.read_csv(file)
+    """Load CSV file and parse Date column."""
+    df = pd.read_csv(file, parse_dates=['Date'], index_col='Date')
+    df = df.asfreq('D')
+    return df
+
