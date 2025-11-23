fa1c143cec483dc1d3699fb50908762891c0c1d0
M-Ghodrat <79672718+M-Ghodrat@users.noreply.github.com>
2025-11-22 16:10:08 -0800

    Delete logic directory

--- logic/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_csv(file):
-    """Load CSV file and parse Date column."""
-    df = pd.read_csv(file, parse_dates=['Date'], index_col='Date')
-    df = df.asfreq('D')
-    return df
-
